c4f5a05e6f0b6f67f02a5aa92ddbb1810f748481
innova1 <tom@teaminnovationllc.com>
2020-09-23 08:51:53 -0400

    Update apAPI.js

--- routes/apAPI.js
+++ routes/apAPI.js
@@ -4,18 +4,22 @@ const db = require('../controllers/dataController');
 
 /* GET agile principles json */
 router.get('/*', function(req, res) {
-  const id = req.params[0];
-  const type = 'manifesto';
-  const principle = db.getPrincipleByID(type, id);
-  const principlesObj = db.getPrinciplesObj();
+      const id = req.params[0];
+      const type = 'manifesto';
+      if(id==0 || id == null) {
+          const principlesObj = db.getPrinciplesObj();
+      } else {
+          const principle = db.getPrincipleByID(type, id);
+      }
+  
 
-  if(id) {
-	res.json({id: id, principle: principlesObj[id].principle });
-  } else {
-	const principles = db.getPrinciplesObj('manifesto');
-	console.log('short desc is ' + principlesObj[1].shortdescription);
-  	res.json({ principlesObject: principlesObj });
-  }
+    if(id) {
+        res.json({id: id, principle: principlesObj[id].principle });
+    } else {
+        const principles = db.getPrinciplesObj('manifesto');
+        console.log('short desc is ' + principlesObj[1].shortdescription);
+        res.json({ principlesObject: principlesObj });
+    }
 });
 
 module.exports = router;
