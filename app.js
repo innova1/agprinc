1bf341d884ff3b6ff72fc986c2f21d48c08ef44a
innova1 <tom@teaminnovationllc.com>
2020-09-24 16:06:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/agileframeworks/', agprisApiRouter);
+app.use('/api/agileframeworks/*', agprisApiRouter);
 app.use('/agileframeworks/', agprisRouter);
 
 // catch 404 and forward to error handler
