8b03a9d57d0a4f2b0fe68ff07353baa4c579c383
innova1 <tom@teaminnovationllc.com>
2020-09-23 22:19:13 -0400

    Update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -15,7 +15,7 @@ exports.getPrinciplesArray = function(type) {
     return getPrinciplesArray(type);
 }
 
-function agetPrinciplesArray(type) {
+function getPrinciplesArray(type) {
     //console.log('in get prin obj export with type ' + type);
     const parray = getDataArray();
     let typeArray = new Array();
