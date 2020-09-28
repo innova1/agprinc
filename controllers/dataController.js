df4398e57769f1c4e2fd008867239bd36a7a0c9f
innova1 <tom@teaminnovationllc.com>
2020-09-27 23:09:03 -0400

    Update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -208,7 +208,7 @@ function createSearchArray() {
         for( const kwd of tempArray ) {
           searchObj = searchMap.get(kwd);
           if(!searchObj) { //the keyword is not already in the map, then add
-            locationObj = { framework: 'what', type: 'where', id: 'who')} };
+            locationObj = { framework: 'what', type: 'where', id: 'who' } };
             searchObj = { keyword: locations.push(locationObj);
             searchMap.set(kwd, searchObj);
           } else { //add location to existing
