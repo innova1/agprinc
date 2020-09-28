b50fe089336af8bde27d830f4cce0a3879817ff8
innova1 <tom@teaminnovationllc.com>
2020-09-28 17:27:52 -0400

    Update agileprinciples.js

--- routes/agileprinciples.js
+++ routes/agileprinciples.js
@@ -29,8 +29,7 @@ router.get('/:framework', function(req, res) {
 router.get('/:framework/search', function(req, res) {
     const framework = req.params.framework;
     const searchterms = req.query.searchterms;
-	//const searchterms = ['customer','development'];
-    console.log('in search with framework-' + framework + ' and searchterms-' + searchterms);
+    console.log('in search with framework-' + framework + ' and searchterms-' + decodeURI(searchterms);
     const frameworksArray = db.getFrameworksArray();
     //console.log('in router get /:framework with ' + framework );
     //console.log('in router get /:framework with first frameworks principle ' + frameworksArray[0] );
