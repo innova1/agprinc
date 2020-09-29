3063643f39e37d99c641a4526e2efd2ebc9d70f4
innova1 <tom@teaminnovationllc.com>
2020-09-29 16:35:33 -0400

    Update agileprinciples.js

--- routes/agileprinciples.js
+++ routes/agileprinciples.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res) {
 });
 
 /* View edit keywords page */
-router.get('/keywords', kwd.displayKeywordList(req, res) );
+router.get('/keywords', kwd.displayKeywordList ;
 
 /* search */
 router.get('/search', function(req, res) {
