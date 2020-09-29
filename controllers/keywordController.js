cb33e48fc1964b528543149dd1fbe936b0ffe967
innova1 <tom@teaminnovationllc.com>
2020-09-29 16:50:25 -0400

    Update keywordController.js

--- controllers/keywordController.js
+++ controllers/keywordController.js
@@ -2,7 +2,7 @@ const db = require('./dataController');
 
 exports.displayKeywordList = function(req, res) {
 	const debug = true;
-	const parray = db.getPrinciplesArray();
+	const parray = db.getPrinciplesArray('','');
 	
 	res.render('viewEntriesKeywords', { title: 'Keywords List', principlesArray: parray });
 }
