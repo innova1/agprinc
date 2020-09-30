fe58ad48696d6031a94796a41d90bae3aeac9608
innova1 <tom@teaminnovationllc.com>
2020-09-30 10:48:15 -0400

    background color of edit and update piping

--- controllers/keywordController.js
+++ controllers/keywordController.js
@@ -12,7 +12,7 @@ exports.updateKeywords = function(req, res) {
     const newkeywords = req.body;
 	const parray = db.getPrinciplesArray('','');
     
-    if(debug) {console.log('will update with ' + newkeywords)};
+    if(debug) {console.log('will update with ' + newkeywords.toString() )};
 	
 	res.render('viewEntriesKeywords', { title: 'Keywords List', principlesArray: parray });
 }
