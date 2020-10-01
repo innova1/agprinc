0177d06bf9d222e50d58bbc53833028f2931bce8
innova1 <tom@teaminnovationllc.com>
2020-10-01 15:57:50 -0400

    Update keywordController.js

--- controllers/keywordController.js
+++ controllers/keywordController.js
@@ -20,16 +20,20 @@ exports.editKeyword = async function(req, res) {
 }
 
 exports.updateKeywords = async function(req, res) {
-	const debug = true;
-    const framework = req.query.framework;
-    const type = req.query.type;
-    const id = req.query.id;
-    const newkeywords = req.body;
-    if(debug) {console.log('in update Keywords with framework:' + framework + ', type:' + type + ', id:' + id + ', newkeywords:' + newkeywords );}
-	const principlesArray = await db.getPrinciplesArray('','');
-    //await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, task);
-    if(debug) {console.log('will update with ' + JSON.stringify(newkeywords) )};
-    db.updateKeywords( framework, type, id, newkeywords.keywords );
+    try {
+        const debug = true;
+        const framework = req.query.framework;
+        const type = req.query.type;
+        const id = req.query.id;
+        const newkeywords = req.body;
+        if(debug) {console.log('in update Keywords with framework:' + framework + ', type:' + type + ', id:' + id + ', newkeywords:' + newkeywords );}
+        const principlesArray = await db.getPrinciplesArray('','');
+        //await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, task);
+        if(debug) {console.log('will update with ' + JSON.stringify(newkeywords) )};
+        db.updateKeywords( framework, type, id, newkeywords.keywords );
+    } catch(err) {
+        console.log('error in updateKeywords ' + err.message );
+    }
 	
 	res.render('viewEntriesKeywords', { title: 'Keywords List', principlesArray: principlesArray });
 }
