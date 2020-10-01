032500c48aad8f7922a5f92fba1689279241ff98
innova1 <tom@teaminnovationllc.com>
2020-10-01 15:53:05 -0400

    Update keywordController.js

--- controllers/keywordController.js
+++ controllers/keywordController.js
@@ -2,25 +2,11 @@ const db = require('./dataController');
 
 exports.displayKeywordList = async function(req, res) {
 	const debug = true;
-	const parray = await db.getPrinciplesArray('','');
-	
-	res.render('viewEntriesKeywords', { title: 'Keywords List', principlesArray: parray });
-}
-
-exports.updateKeywords = async function(req, res) {
-	const debug = true;
-    const framework = req.query.framework;
-    const type = req.query.type;
-    const id = req.query.id;
-    const newkeywords = req.body;
-    if(debug) {console.log('in update Keywords with framework:' + framework + ', type:' + type + ', id:' + id + ', newkeywords:' + newkeywords );}
 	const principlesArray = await db.getPrinciplesArray('','');
-    
-    if(debug) {console.log('will update with ' + JSON.stringify(newkeywords) )};
-    db.updateKeywords( framework, type, id, newkeywords.keywords );
 	
 	res.render('viewEntriesKeywords', { title: 'Keywords List', principlesArray: principlesArray });
 }
+
 
 exports.editKeyword = async function(req, res) {
 	const debug = true;
@@ -31,4 +17,19 @@ exports.editKeyword = async function(req, res) {
 	const principlesArray = await db.getPrincipleByID(framework, type, id);
     
     res.render('editEntryKeyword', { title: 'Edit Keyword', principleObj: principlesArray[0] });
+}
+
+exports.updateKeywords = async function(req, res) {
+	const debug = true;
+    const framework = req.query.framework;
+    const type = req.query.type;
+    const id = req.query.id;
+    const newkeywords = req.body;
+    if(debug) {console.log('in update Keywords with framework:' + framework + ', type:' + type + ', id:' + id + ', newkeywords:' + newkeywords );}
+	const principlesArray = await db.getPrinciplesArray('','');
+    //await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, task);
+    if(debug) {console.log('will update with ' + JSON.stringify(newkeywords) )};
+    db.updateKeywords( framework, type, id, newkeywords.keywords );
+	
+	res.render('viewEntriesKeywords', { title: 'Keywords List', principlesArray: principlesArray });
 }
