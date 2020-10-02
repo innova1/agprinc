5ace5e792dad047bcaf734d1ab607fde9c75739d
innova1 <tom@teaminnovationllc.com>
2020-10-02 09:06:12 -0400

    Update startag.sh

--- bin/startag.sh
+++ bin/startag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo forever start -a -l /home/bitnami/projects/agileprinciples/agprinc/logs/forever.log -o /home/bitnami/projects/agileprinciples/agprinc/logs/fbk.log -e /home/bitnami/projects/agileprinciples/agprinc/logs/logs/err.log ./bin/www
+sudo forever start -a -l /home/bitnami/projects/agileprinciples/agprinc/logs/forever.log -o /home/bitnami/projects/agileprinciples/agprinc/logs/agp.log -e /home/bitnami/projects/agileprinciples/agprinc/logs/err.log ./bin/www
