f1bfc44f68c7f38f63c09060f684446b4709efb1
innova1 <tom@teaminnovationllc.com>
2020-10-08 16:16:54 -0400

    Update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -223,7 +223,7 @@ exports.getKeywordMatch = async function a( searchText ) {
 			var elstring = element.keyword + '';
 			var str = elstring.substring(0, searchText.length);
 			//if(debug) { console.log( 'comparing ' + str + ' with ' + searchText ) };
-			if(str==searchText) {
+			if( str.toUpperCase()==searchText.toUpperCase() ) {
 				if(debug) { console.log( 'pushing compared ' + str + ' with ' + searchText ) };
 				result.push(elstring); 
 			}
