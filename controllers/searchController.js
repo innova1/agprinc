51ea6d96732d9c4b624505b22d63c3782c655178
innova1 <tom@teaminnovationllc.com>
2020-10-11 00:33:29 -0400

    Update searchController.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -28,7 +28,7 @@ exports.getSearchTerms = async function(req, res) {
 }
 
 exports.getItems = async function(req, res) {
-	const debug = true;
+	const debug = false;
     const framework = req.params.framework;
 	if(debug) { console.log('in / in searchController.getItems(' + framework + ')')};
     const itemsArray = await db.getPrinciplesArray(framework,'');
