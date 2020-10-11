de0bd5cd1a7e0270e8ae81fb1703924fb2d95872
innova1 <tom@teaminnovationllc.com>
2020-10-11 19:28:29 -0400

    Update agpv.js

--- public/javascripts/agpv.js
+++ public/javascripts/agpv.js
@@ -160,7 +160,7 @@ function createCurrentSearchTermsObject(searchterms) {
 	return currentTermsObject;
 }
 
-const currentSearchTermsObject;
+const currentSearchTermsObject = "";
 
 function populateCurrentSearchTermsDiv(searchterms) {
 	/*
@@ -172,7 +172,7 @@ function populateCurrentSearchTermsDiv(searchterms) {
 	});
 	*/
 	console.log('creating terms object');
-	if(!currentSearchTermsObject) currentSearchTermsObject = createCurrentSearchTermsObject(searchterms);
+	if(!currentSearchTermsObject=="") currentSearchTermsObject = createCurrentSearchTermsObject(searchterms);
 	document.getElementById('removetermlinks').innerHTML = termsObj.getCurrentTermsHtml();
 	document.getElementById('currentsearchterms').innerHTML = termsObj.getCurrentTerms();
 	document.getElementById('suggestions').style.display = 'none';
