60a5b7b0d3b9a72ea56a92b677902675a8683938
innova1 <tom@teaminnovationllc.com>
2020-10-12 18:55:47 -0400

    Update agpv.js

--- public/javascripts/agpv.js
+++ public/javascripts/agpv.js
@@ -11,7 +11,7 @@ as an exercise later can try to figure out how to make this work with async awai
 var isSmallViewport = window.matchMedia("(max-width: 1000px)");
 
 var selF = document.getElementById('selectedFramework').innerHTML;
-//setSelected(selF);
+setSelected(selF);
 
 var searchRequest = null;
 var suggElement = document.getElementById('suggestions');
@@ -113,7 +113,7 @@ async function replaceItemsPanels(framework) {
 		dataType: "json",
 		success: function(result) {
 			populateItemsPanels(result.items);
-			//setSelected(framework);
+			setSelected(framework);
 			isSmallViewport.addListener(setMenuCollapsed);
 			setMenuCollapsed(isSmallViewport);
 		}
