b172c3f7f622cfef1931a89d196d1c12cf097fc1
innova1 <tom@teaminnovationllc.com>
2020-10-13 22:07:25 -0400

    Update agpv.js

--- public/javascripts/agpv.js
+++ public/javascripts/agpv.js
@@ -368,7 +368,7 @@ $("form").on("submit", function (e) {
 		dataType: "json",
 		success: function(result) {
 			//console.log('result: ' + result.searchWords[0])
-			const found = result.searchWords.find( s => s.toUpperCase() === searchtext.toUpperCase() );
+			const found = result.searchWords.find( (s) => { s.toUpperCase() === searchtext.toUpperCase() } );
 			//console.log('compared ' + s.toUpperCase() + ' to ' + searchtext.toUpperCase() );
 			console.log('found: ' + found);
 			if(found) {
