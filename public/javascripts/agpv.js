563f27f9b286b9b53da24953db3326832cd9a7fa
innova1 <tom@teaminnovationllc.com>
2020-10-21 12:33:44 -0400

    Update agpv.js

--- public/javascripts/agpv.js
+++ public/javascripts/agpv.js
@@ -417,11 +417,11 @@ $(function() {
 	$("#searchtext").keyup(function() {
 		var oldThis = this,
 		value = $(this).val();
+		var framework = getCurrentFramework();
 		if(debug) console.log("value is " + value + ", framework is " + framework);
 
 		if (value.length >= minlength ) {
 			if (searchRequest != null) { searchRequest.abort(); }
-			var framework = getCurrentFramework();
 			if(debug) console.log( "in $ function after value check--framework is " + framework)
 			if(debug) console.log("about to ajax with framework=" + framework + " and value=" + value);
 			//if(debug) console.log("field sel frmwrk: " + $('#selectedFramework').html());
