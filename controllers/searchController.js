6bc536d6291e2ef8fdb02e8d20f838d741a30e27
innova1 <tom@teaminnovationllc.com>
2020-10-24 11:40:21 -0400

    Update searchController.js

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -7,7 +7,7 @@ exports.getSearchWords = async function(req, res) {
 	if(debug) console.log('in search with searchtext-' + decodeURI(searchtext).replace(/\s/g, '+'));
 	var searchWordsArray = new Array();
 	try {
-		searchWordsArray = await db.getKeywordMatch(framework, searchtext);
+		searchWordsArray = await getKeywordMatch(framework, searchtext);
 	} catch(err) {
 		console.log("error in searchcontroller.getSearchWords with " + err);
 	}
