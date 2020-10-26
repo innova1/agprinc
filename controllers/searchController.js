cb557a4603c3e32addf4bd4b47630fb3f1c70710
innova1 <tom@teaminnovationllc.com>
2020-10-26 18:02:33 -0400

    testing with mocha

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -75,11 +75,8 @@ async function getItemsFilteredByKeywords( framework, searchWordsArray ) {
 	var foundItems = new Array();
     try {
         const dataArray = await db.getPrinciplesArray('all','');
-		if(debug) console.log('db.getFilteredItems just before create search map');
         const searchMap = await db.getSearchMap(framework);
-		if(debug) console.log('db.getFilteredItems just after create search map. searchMap length: ' + searchMap.size);
 		foundItems = collectItemsMatchingSearchTerms(searchMap, dataArray, searchWordsArray);
-        if(debug) if(foundItems[0]) { console.log('first found item is ' + foundItems[0].framework) } else { console.log('none found') };
         
     } catch(err) {
         console.log('error in getItemsFilteredByKeywords ' + err.message );
--- test/testag.js
+++ test/testag.js
@@ -13,10 +13,19 @@ describe('times5()', function() {
 });
 
 describe('times6()', function() {
-	it('should return the number time 6', function() {
+	it('should return the number times 6', function() {
 		assert.equal( times6(4), 24);
 	});
 });
+
+var isLocationAlreadyInArray = app.__get__('isLocationAlreadyInArray');
+describe('isLocationAlreadyInArray', function() {
+	it('should return -1', function() {
+		var loc = new object();
+		loc.index = 1;
+		assert.equal( isLocationAlreadyInArray(['zerod','first','second','third'], loc))
+	})
+})
 
 /*
 describe('Array', function() {
