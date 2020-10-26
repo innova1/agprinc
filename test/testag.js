b5fd9bcd05ee675aeb189a53b271d7e6abb91349
innova1 <tom@teaminnovationllc.com>
2020-10-26 17:37:43 -0400

    Update testag.js

--- test/testag.js
+++ test/testag.js
@@ -1,11 +1,13 @@
 var assert = require('assert');
 var srch = require('../controllers/searchController')
 
+/*
 describe('times5()', function() {
 	it('should return the number times 5', function() {
 		assert.equal( srch.times5(4), 20);
 	});
 });
+*/
 
 describe('times6()', function() {
 	if('should return the number time 6', function() {
