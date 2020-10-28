fc4fc231d6bd2149076db0924e70f5942e2333ee
innova1 <tom@teaminnovationllc.com>
2020-10-28 16:31:13 -0400

    Update testag.js

--- test/testag.js
+++ test/testag.js
@@ -28,43 +28,44 @@ describe('addItem', function() {
 	var item2 = { id: '2', framework: 'frame2', type: 'type2' }
 	var dataArray = [item1, item2];
 	var itemFinder = new ItemFinder();
-	it('should return true', function() {
+	
+	it('should return an object', function() {
 		itemFinder = new ItemFinder(item1.framework, item1.type, item1.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		var test = foundItems.find( e => e.id == itemFinder.ordinal && e.framework == itemFinder.framework && e.type == itemFinder.type);
 		assert.equal( typeof test, 'object')
 	});
 	
-	it('should still only have 1 element so return false', function() {
+	it('should not have 2 elements even though call was made twice', function() {
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.notEqual( foundItems.length, 2)
 	});
 	
-	it('should have 2 elements now so return true', function() {
+	it('should have 2 elements now because item was changed slightly', function() {
 		itemFinder = new ItemFinder(item2.framework, item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 2)
 	});
 	
-	it('should have 3 elements now so return true', function() {
+	it('should have 3 elements now because item was changed slightly', function() {
 		itemFinder = new ItemFinder(item2.framework, item2.type, 3);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 3)
 	});
 	
-	it('should have 4 elements now so return true', function() {
+	it('should have 4 elements now because item was changed slightly', function() {
 		itemFinder = new ItemFinder(item2.framework, 'type3', item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 4)
 	});
 	
-	it('should have 5 elements now so return true', function() {
+	it('should have 5 elements now because item was changed slightly', function() {
 		itemFinder = new ItemFinder('frame3', item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 5)
 	});
 	
-	it('should still have only 5 elements so return false', function() {
+	it('should not have 6 elements even though call was made again', function() {
 		itemFinder = new ItemFinder('frame3', item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.notEqual( foundItems.length, 6)
