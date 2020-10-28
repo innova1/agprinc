5e1424141524614c783035bbce06e213dcbd2062
innova1 <tom@teaminnovationllc.com>
2020-10-28 16:05:03 -0400

    Update testag.js

--- test/testag.js
+++ test/testag.js
@@ -31,15 +31,7 @@ describe('addItem', function() {
 	it('should return true', function() {
 		itemFinder = new ItemFinder(item1.framework, item1.type, item1.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
-		
-		console.log('item1:' + item1.id + ',' + item1.framework + ',' + item1.type)
-		console.log('in itemfinder:' + itemFinder.ordinal + ',' + itemFinder.framework + ',' + itemFinder.type)
-		console.log('in dataArray:' + dataArray[0].id + ',' + dataArray[0].framework + ',' + dataArray[0].type)
-		
 		var test = foundItems.find( e => e.id == itemFinder.ordinal && e.framework == itemFinder.framework && e.type == itemFinder.type);
-		
-		console.log('test:' + test)
-		
 		assert.equal( typeof test, 'object')
 	});
 	
@@ -48,8 +40,7 @@ describe('addItem', function() {
 		assert.equal( foundItems.length, 1)
 	});
 	
-	
-	it('should have 2 elements so return true', function() {
+	it('should have 2 elements now so return true', function() {
 		itemFinder = new ItemFinder(item2.framework, item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 2)
