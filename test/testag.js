006a88678cd8eb16f597e73aac681e2754d46d1f
innova1 <tom@teaminnovationllc.com>
2020-10-28 16:08:53 -0400

    Update testag.js

--- test/testag.js
+++ test/testag.js
@@ -48,6 +48,7 @@ describe('addItem', function() {
 	
 	it('should have 3 elements now so return true', function() {
 		itemFinder.ordinal = 3
+		console.log(itemFinder.key)
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 3)
 	});
