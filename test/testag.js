2d013b88afaed999d7309f9aedac23f2b14908b4
innova1 <tom@teaminnovationllc.com>
2020-10-28 15:59:05 -0400

    Update testag.js

--- test/testag.js
+++ test/testag.js
@@ -30,19 +30,21 @@ describe('addItem', function() {
 	var itemFinder = new ItemFinder(item1.framework, item1.type, item1.id);
 	it('should return true', function() {
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
+		
 		console.log('item1:' + item1.id + ',' + item1.framework + ',' + item1.type)
 		console.log('in itemfinder:' + itemFinder.ordinal + ',' + itemFinder.framework + ',' + itemFinder.type)
 		console.log('in dataArray:' + dataArray[0].id + ',' + dataArray[0].framework + ',' + dataArray[0].type)
+		
 		var test = foundItems.find( e => e.id == itemFinder.ordinal && e.framework == itemFinder.framework && e.type == itemFinder.type);
+		
 		console.log('test:' + test)
+		
 		assert.equal( typeof test, 'object')
 	});
 	
-	it('should return false', function() {
-		var loc = new Object();
-		loc.index = 'fourth';
-		var arr = ['zerod','first','second','third']
-		assert.equal( isLocationAlreadyInArray(arr, loc), false)
+	it('should still only have 1 element so return true', function() {
+		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
+		assert.equal( foundItems.length, 1)
 	});
 });
 
