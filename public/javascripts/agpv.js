296d7f5b66c78ffb38b31a8346f484b8d8fca1c3
innova1 <tom@teaminnovationllc.com>
2020-10-29 08:56:19 -0400

    Update agpv.js

--- public/javascripts/agpv.js
+++ public/javascripts/agpv.js
@@ -425,7 +425,7 @@ $("form").on("submit", function (e) {
 	e.preventDefault();
 });
 
-$('#selectedFramework')on("onchange", function(e) {
+$('#selectedFramework').on("onchange", function(e) {
 	console.log('selected was changed to ' + $('#selected').text() );
 });
 
