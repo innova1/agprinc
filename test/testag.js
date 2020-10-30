5bd71a1f5aac19673ea8123a4d8dfb29b55dbee7
innova1 <tom@teaminnovationllc.com>
2020-10-30 15:06:00 -0400

    Update testag.js

--- test/testag.js
+++ test/testag.js
@@ -20,10 +20,10 @@ describe('addItem', function() {
 	var item1 = { id: '1', framework: 'frame1', type: 'type1' }
 	var item2 = { id: '2', framework: 'frame2', type: 'type2' }
 	var dataArray = [item1, item2];
-	var itemFinder = new ItemFinder();
+	var itemFinder = new srch.ItemFinder();
 	
 	it('should return an object', function() {
-		itemFinder = new ItemFinder(item1.framework, item1.type, item1.id);
+		itemFinder = new srch.ItemFinder(item1.framework, item1.type, item1.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		var test = foundItems.find( e => e.id == itemFinder.ordinal && e.framework == itemFinder.framework && e.type == itemFinder.type);
 		assert.equal( typeof test, 'object')
@@ -35,31 +35,31 @@ describe('addItem', function() {
 	});
 	
 	it('should have 2 elements now because item was changed slightly', function() {
-		itemFinder = new ItemFinder(item2.framework, item2.type, item2.id);
+		itemFinder = new srch.ItemFinder(item2.framework, item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 2)
 	});
 	
 	it('should have 3 elements now because item was changed slightly', function() {
-		itemFinder = new ItemFinder(item2.framework, item2.type, 3);
+		itemFinder = new srch.ItemFinder(item2.framework, item2.type, 3);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 3)
 	});
 	
 	it('should have 4 elements now because item was changed slightly', function() {
-		itemFinder = new ItemFinder(item2.framework, 'type3', item2.id);
+		itemFinder = new srch.ItemFinder(item2.framework, 'type3', item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 4)
 	});
 	
 	it('should have 5 elements now because item was changed slightly', function() {
-		itemFinder = new ItemFinder('frame3', item2.type, item2.id);
+		itemFinder = new srch.ItemFinder('frame3', item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.equal( foundItems.length, 5)
 	});
 	
 	it('should still have only 5 items because 2nd attempt to add same item should be blocked', function() {
-		itemFinder = new ItemFinder('frame3', item2.type, item2.id);
+		itemFinder = new srch.ItemFinder('frame3', item2.type, item2.id);
 		addItem(foundItems, alreadyFoundKeys, dataArray, itemFinder)
 		assert.notEqual( foundItems.length, 6)
 	});
@@ -74,7 +74,7 @@ describe('collectItemsMatchingSearchTerms', function() {
 	var alreadyFoundKeys = ['one','two'];
 	var item1 = { id: '1', framework: 'frame1', type: 'type1' }
 	var item2 = { id: '2', framework: 'frame2', type: 'type2' }
-	var itemFinder = new ItemFinder('frame1','type1','1');
+	var itemFinder = new srch.ItemFinder('frame1','type1','1');
 	var itemFinders = [itemFinder];
 	var keywordItemFinderMap = new Map();
 	var searchObj = {itemFinders: itemFinders}
