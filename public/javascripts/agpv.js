546eab5ba8cb7890f062302d66dec70deeb25901
innova1 <tom@teaminnovationllc.com>
2020-11-02 17:12:04 -0500

    Update agpv.js

--- public/javascripts/agpv.js
+++ public/javascripts/agpv.js
@@ -380,7 +380,7 @@ $(function() {
 
 		if (value.length >= minlength ) {
 			/*experimenting with the lookup object*/
-			console.log(keywords.size())
+			console.log(keywords.get(value))
 			
 			var framework = getCurrentFramework();
 			if (searchRequest != null) { searchRequest.abort(); }
