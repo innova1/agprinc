78ee5bae142d4c6ecebfcfad8a78301da464da2a
innova1 <tom@teaminnovationllc.com>
2020-11-01 21:02:06 -0500

    adding a main landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'This site hosts several applications.', mainurl: '/agileframeworks/', alink: 'You can try this one.' });
 });
 
 module.exports = router;
