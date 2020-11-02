c9071707fbeea2e342152994d37782cdf86b3d98
innova1 <tom@teaminnovationllc.com>
2020-11-01 21:04:26 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This site hosts several applications.', mainurl: '/agileframeworks/', alink: 'You can try this one.' });
+  res.render('index', { title: 'Welcome', message: 'This site hosts several applications.', mainurl: '/agileframeworks/', alink: 'Feel free to try this one.' });
 });
 
 module.exports = router;
