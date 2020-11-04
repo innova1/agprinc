a86a7f352b9caf72474b313e2c5092c524235efa
innova1 <tom@teaminnovationllc.com>
2020-11-04 10:36:17 -0500

    Update agileprinciples.js

--- routes/agileprinciples.js
+++ routes/agileprinciples.js
@@ -1,30 +1,30 @@
-'use strict;'
+'use strict';
 
 var express = require('express');
-//var app = express();
+// var app = express();
 var router = express.Router();
 const db = require('../controllers/dataController');
 const kwd = require('../controllers/keywordController');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-//const tools = require('../controllers/tools');
+// const tools = require('../controllers/tools');
 
 router.use(cookieParser());
 router.use(session({
-	secret: '4$dcf#',
-	resave: true,
-	saveUninitialized: true
+  secret: '4$dcf#',
+  resave: true,
+  saveUninitialized: true
 }));
 
 var Users = [];
 
-//this route get is here to block real signup --remove to let work
+// this route get is here to block real signup --remove to let work
 router.get('/signup', function(req, res) {
-	res.redirect('/agileframeworks');
+  res.redirect('/agileframeworks');
 });
 
 router.post('/signup', function(req, res) {
-	res.redirect('/agileframeworks');
+  res.redirect('/agileframeworks');
 });
 
 router.get('/signup', function(req, res){
@@ -32,33 +32,33 @@ router.get('/signup', function(req, res){
 });
 
 router.post('/signup', function(req, res){
-	console.log('in signup')
-	if(!req.body.id || !req.body.password) {
-		res.status("400");
-		res.send("Invalid details!");
-	} else {
-		Users.filter( function(user) {
-			if(user.id === req.body.id) {
-				res.render('signup', { message: "User Already Exists! Login or choose another user id" });
-			}
-	});
-	var newUser = {id: req.body.id, password: req.body.password};
-	Users.push(newUser);
-	req.session.user = newUser;
-	//res.redirect('/agileframeworks') //temp to not all to keywords
-	res.redirect('/agileframeworks/keywords');
+  console.log('in signup')
+  if(!req.body.id || !req.body.password) {
+    res.status("400");
+    res.send("Invalid details!");
+  } else {
+    Users.filter( function(user) {
+  if(user.id === req.body.id) {
+        res.render('signup', { message: "User Already Exists! Login or choose another user id" });
+      }
+  });
+  var newUser = {id: req.body.id, password: req.body.password};
+  Users.push(newUser);
+  req.session.user = newUser;
+  // res.redirect('/agileframeworks') // temp to not all to keywords
+  res.redirect('/agileframeworks/keywords');
    }
 });
 
 function checkSignIn(req, res, next){
    if(req.session.user){
-	   next();     //If session exists, proceed to page 
+     next();     // If session exists, proceed to page 
    } else {
-		var err = new Error("Not logged in!");
-		console.log(req.session.user);
-		//next(err);  //Error, trying to access unauthorized page! 
-		//res.render('signup', { message: "Sign up for access."}); //temporary fail
-	   res.render('unknown', { title: "Unknown url", message: "You've reached a nonexistant page." });
+    var err = new Error("Not logged in!");
+    console.log(req.session.user);
+    // next(err);  // Error, trying to access unauthorized page! 
+    // res.render('signup', { message: "Sign up for access."}); // temporary fail
+     res.render('unknown', { title: "Unknown url", message: "You've reached a nonexistant page." });
    }
 }
 
@@ -67,29 +67,29 @@ router.get('/login', function(req, res){
 });
 
 router.post('/login', function(req, res){
-	console.log("users: " + Users);
-	if( !req.body.id || !req.body.password ) {
-		res.render('login', { message: "Please enter both id and password" });
-	} else {
-		console.log('id:'+req.body.id+', pwd:'+req.body.password)
-		//added temporarily to let just me login hard coded
-		if(req.body.id === 'tboulet' && req.body.password === 'app44word') {
-			console.log('match')
-			req.session.user = '{id: "tboulet", password: "app44word" }';
-			res.redirect( '/agileframeworks/keywords' );
-		} else {
-		/* commented out temporarily ***
-		Users.filter( function(user) {
-			if(user.id === req.body.id && user.password === req.body.password) {
-				console.log('match')
-				req.session.user = user;
-				res.redirect( '/agileframeworks/keywords' );
-			}
-		});
-		*/
-			res.render('login', { message: "Invalid credentials!"} );
-		}
-	}
+  console.log("users: " + Users);
+  if( !req.body.id || !req.body.password ) {
+    res.render('login', { message: "Please enter both id and password" });
+  } else {
+    console.log('id:'+req.body.id+', pwd:'+req.body.password)
+    // added temporarily to let just me login hard coded
+    if(req.body.id === 'tboulet' && req.body.password === 'app44word') {
+      console.log('match')
+      req.session.user = '{id: "tboulet", password: "app44word" }';
+      res.redirect( '/agileframeworks/keywords' );
+    } else {
+    /* commented out temporarily ***
+    Users.filter( function(user) {
+      if(user.id === req.body.id && user.password === req.body.password) {
+        console.log('match')
+        req.session.user = user;
+        res.redirect( '/agileframeworks/keywords' );
+      }
+    });
+    */
+      res.render('login', { message: "Invalid credentials!"} );
+    }
+  }
 });
 
 router.get('/logout', function(req, res){
@@ -120,20 +120,20 @@ router.get('/keywordEdit', checkSignIn, kwd.editKeyword);
 router.post('/keywordUpdate', checkSignIn, kwd.updateKeywords);
 
 router.get('/', function(req, res) {
-	res.render('agprisSelectedFrameworkBoot');
+  res.render('agprisSelectedFrameworkBoot');
 });
 
 /* GET one framework of agile principles */
 router.get('/:framework', async function(req, res) {
-    //console.log('in router get /:framework');
+    // console.log('in router get /:framework');
     const framework = req.params.framework;
     const frameworksArray = await db.getFrameworksArray();
-    //console.log('in router get /:framework with ' + framework );
-    //console.log('in router get /:framework with first frameworks principle ' + frameworksArray[0] );
+    // console.log('in router get /:framework with ' + framework );
+    // console.log('in router get /:framework with first frameworks principle ' + frameworksArray[0] );
     const principlesArray = await db.getPrinciplesArray(framework, '');
     const keywordItemFinderMap = await db.keywordItemFinderMap(); 
-    //console.log('in router: len is ' + keywordItemFinderMap.length);
-    //console.log('in :framework ' + principlesArray[1].text);
+    // console.log('in router: len is ' + keywordItemFinderMap.length);
+    // console.log('in :framework ' + principlesArray[1].text);
     res.render('agprisSelectedFrameworkBoot', { title: 'Agile Principles', frameworks: frameworksArray, framework: framework, principlesArray: principlesArray, keywordItemFinderMap: keywordItemFinderMap });
 });
 
@@ -141,7 +141,7 @@ router.get('/:framework/:type', async function(req, res) {
     const framework = req.params.framework;
     const type = req.params.type;
     const frameworksArray = await db.getFrameworksArray();
-    //console.log('in router get /:framework with ' + framework + ", " + type );
+    // console.log('in router get /:framework with ' + framework + ", " + type );
     const principlesArray = await db.getPrinciplesArray(framework, type);
     const keywordItemFinderMap = await db.keywordItemFinderMap(); 
     res.render('agprisSelectedFramework', { title: 'Agile Principles', frameworks: frameworksArray, framework: framework, type: type, principlesArray: principlesArray, keywordItemFinderMap: keywordItemFinderMap });
@@ -164,7 +164,7 @@ router.get('/:framework/:type/:id', function(req, res) {
 });
 
 router.get('/**', function(req, res) {
-	res.render('unknown', { title: "Unknown url", message: "You've reached a nonexistant page." });
+  res.render('unknown', { title: "Unknown url", message: "You've reached a nonexistant page." });
 });
 
 module.exports = router;
