148e16755f1aafa18e43b9533d74648b7d96fc45
innova1 <tom@teaminnovationllc.com>
2020-11-04 13:25:52 -0500

    Update agileprinciples.js

--- routes/agileprinciples.js
+++ routes/agileprinciples.js
@@ -155,14 +155,14 @@ router.get('/:framework/:type/:id', function(req, res) {
   const type = req.params.type;
   const id = req.params.id;
 
-if ( !isNaN(id) ) {
-  if (db.isIDInRange(framework, type, id)) {
-    const principleObj = db.getPrincipleByID(framework, type, id);
-    res.json({id: id, principle: principleObj.text });
-  } else {
-    res.render('agpris', {id: id, principle: "error: id out of range"});
-  }
-} 
+  if ( !isNaN(id) ) {
+    if (db.isIDInRange(framework, type, id)) {
+      const principleObj = db.getPrincipleByID(framework, type, id);
+      res.json({id: id, principle: principleObj.text });
+    } else {
+      res.render('agpris', {id: id, principle: "error: id out of range"});
+    }
+  } 
 });
 
 router.get('/**', function(req, res) {
