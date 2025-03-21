39efd66b3fa025ef9f9acbc9bc2cf064f8817a3d
innova1 <tom@teaminnovationllc.com>
2025-03-21 18:00:52 -0400

    update locally changes files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome', message: 'This site hosts several applications.', mainurl: '/agileframeworks/', alink: 'Feel free to try this one.' });
+  res.render('index', { title: 'Welcome', message: 'This site hosts several applications.', mainurl: '/agileframeworks/', alink: 'Feel free to try this one.', message1: 'My website: ', mainurl2: 'https://tomboulet.com', alink2: 'tomboulet.com' });
 });
 
 module.exports = router;
